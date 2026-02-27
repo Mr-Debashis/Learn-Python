b29347af0103dec4d892f707c9fca36ee6b8dcc1
Debashis Patra <debashispatra0992@gmail.com>
2026-02-27 23:03:05 +0530

    02_python_output
    02_python_output

---
+++ 01_basics/02_python_output.py
@@ -0,0 +1,13 @@
+# print() function to display text or output values:
+
+print("Debashis Patra")
+
+# Text in Python must be inside quotes. You can use either " double quotes or ' single quotes, without quotes its gives error for string.
+# But for number values don't needed quotes.
+
+print(3)
+print(358)
+
+
+
+print("I am", 35, "years old.")
