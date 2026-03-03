d66a511974eeb3295d4adb8bbd355bf20adeb0c4
Debashis Patra <debashispatra0992@gmail.com>
2026-03-03 20:56:23 +0530

    02_numbers
    02_numbers

---
+++ 02_datatypes/02_numbers.py
@@ -0,0 +1,11 @@
+# There are three numeric types in Python:
+
+# 1. int          x = 1    int
+# 2. float        y = 2.8  float
+# 3. complex      z = 1j   complex
+
+# Int:---     Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+# Float:---   Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+# Complex:--- Complex numbers are written with a "j" as the imaginary part:
+ 
+# We can convert from one type to another with the int(), float(), and complex() methods.
