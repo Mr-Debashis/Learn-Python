f6a546087bae7b4e8749bcc730e5deacf8339a88
Debashis Patra <debashispatra0992@gmail.com>
2026-03-13 23:09:35 +0530

    01_if
    01_if

---
+++ 03_conditional_statement/01_if.py
@@ -0,0 +1,19 @@
+
+
+# Python supports the usual logical conditions from mathematics:
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+# These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# The if statement evaluates a condition (an expression that results in True or False). If the condition is true, the code block inside the if statement is executed. If the condition is false, the code block is skipped.
