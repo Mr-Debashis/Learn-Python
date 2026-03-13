cb9b572667853225def9a776b2590e26649c17cb
Debashis Patra <debashispatra0992@gmail.com>
2026-03-13 23:07:43 +0530

    tuple
    tuple

---
+++ 02_datatypes/06_tuple.py
@@ -0,0 +1,8 @@
+
+# Tuples is same like List, but it's decleared by using (). All other things are same as list.
+
+# Tuples is Imutable or Unchangeable.
+
+x = ("apple", "banana", "cherry")
+
+# If we learn about list, then we can use Tuple also. All methods are same as list.
