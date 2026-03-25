6da8ba2143a189107e1fc9770882390baaa14779
Debashis Patra <debashispatra0992@gmail.com>
2026-03-25 20:47:27 +0530

    Delete 04_iterations/02_while_loop.py

--- 04_iterations/02_while_loop.py
+++
@@ -1,9 +0,0 @@
-
-
-# +++++++++++++++++++++++++++++++ Python While Loop +++++++++++++++++++++++++++++++++++++++++
-
-# With the while loop we can execute a set of statements as long as a condition is true.
-i = 1
-while i < 6:
-  print(i)
-  i += 1
